f0814493dbf01ba1562f12eaae630464b05206ff
Muhammad Rizky Nugraha <74727636+kykylextric@users.noreply.github.com>
2022-01-20 12:46:32 +0700

    Add files via upload

---
+++ Tugas Akhir - Minggu 14/js/script.js
@@ -0,0 +1,17 @@
+let hamberger = document.querySelector('.hamberger');
+
+
+function runPopup(){
+    window.alert("Koneksikan Wallet anda terlebih dahulu");
+};
+
+function runPopup2(){
+  if(window.confirm("Sudahkah Wallet Digital anda terhubung dengan Andalas NFT?"))
+     {
+       window.alert("Selamat, anda bisa menjelajah marketplace");
+     }
+     else
+     {
+       window.alert("Wallet gagal terhubung");
+     }};
+    
